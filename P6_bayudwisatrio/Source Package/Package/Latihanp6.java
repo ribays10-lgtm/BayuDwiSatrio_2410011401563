5defab583abfe29b05716837d6f456228ff517fe
ribays10-lgtm <ribays10@gmail.com>
2026-04-14 12:50:17 +0700

    Add files via upload

---
+++ P6_bayudwisatrio/Source Package/Package/Latihanp6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Latihanp6 {
+    public static void main (String[] args){
+        Scanner input=new Scanner(System.in);
+        
+        System.out.print("Masukan nilai :");
+        int nilai=input.nextInt();
+
+        if (nilai>= 75){
+            System.out.println("Status : Lulus");
+        }
+        else{
+            
+        }
+    }
+}
